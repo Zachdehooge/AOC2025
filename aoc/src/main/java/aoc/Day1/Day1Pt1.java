cb7b2906433f395a4b185687a12db37093692935
zachdehooge <Zacharydehooge@gmail.com>
2025-12-01 12:26:25 -0500

    Initial Pt1 Day 1

---
+++ aoc/src/main/java/aoc/Day1/Day1Pt1.java
@@ -0,0 +1,37 @@
+package main.java.aoc.Day1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day1Pt1 {
+
+    static void main() {
+        ArrayList<String> direction = new ArrayList<>();
+        ArrayList<String> number = new ArrayList<>();
+
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("aoc/src/main/java/aoc/Day1/input.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                //System.out.println(line);
+                char determineDir = line.charAt(0);
+                String determineNum = line.substring(1);
+                //System.out.println(determineDir);
+                direction.add(String.valueOf(determineDir));
+                number.add(determineNum);
+
+                line = reader.readLine();
+            }
+            reader.close();
+            System.out.println(direction);
+            System.out.println("\n" + number);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
